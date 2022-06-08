505486dda935f1fbec95068439978bc9662ae9ff
kwonilgun <kwonilgun@naver.com>
2022-06-08 18:02:35 +0900

    Initial Commit

---
+++ SwiftLanguage/SwiftLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLanguageApp.swift
+//  SwiftLanguage
+//
+//  Created by kwonilgun on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
